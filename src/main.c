f017727683137cc4e6d69a237c3893f86c503eec
Taumicron <73892287+Taumicron@users.noreply.github.com>
2020-11-24 13:44:57 +0100

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <SDL.h>
+
+int main(int argc, char **argv)
+{
+    SDL_version nb;
+    SDL_VERSION(&nb);
+
+    printf("SDL : %d.%d.%d\n",nb.major,nb.minor,nb.patch);
+    system("PAUSE");
+    return 0;
+}
