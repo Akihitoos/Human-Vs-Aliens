089029b16c2da336a662cc9c0091c71aaaddb3d9
cryptocas <luk.v2111@gmail.com>
2020-11-28 03:25:44 +0100

    ajout de code pour interaction initx2 + addentity(nonfini)

--- src/interaction.c
+++ src/interaction.c
@@ -1,6 +1,82 @@
 #include <interaction.h>
-#include <human.h>
-#include<Alien.h>
+#include <Entity.h>
 
+Entity ***initTab_Human(){
+    Entity *** p =NULL; 
+    int column = 0;
+    int row =5;
+    p=(Entity***)calloc(row,sizeof(Entity**));
+    if(p!=NULL){
+        return p;
+    }
+    else{
+        fprintf(stderr,"Allocation error in initTab_Human");
+    }
+}
 
+Entity ***initTab_Alien(){
 
+    Entity *** p =NULL;
+    int column = 0;
+    int row =5;
+    p=(Entity ***)calloc(row, sizeof(Entity**));
+    if(p!=NULL){
+        return p;
+    }
+    else{
+        fprintf(stderr, "Allocation error in initTab_Alien");
+    }
+}
+
+void add_entity()
+{
+    int ch1, ch2;
+    printf("ALIEN OU HUMAIN ?(1 ou 0)");
+    switch (ch1)
+    {
+    case 1:
+        Entity * a = NULL;
+        printf("que voulez ajoutez comme Alien");
+        printf("1 alien distance");
+        prinf("2 alien melee");
+        printf("3 alien tank");
+        switch (ch2){
+            case 1:
+                
+                break;
+            case 2:
+
+                break;
+            case 3  :
+
+                break;
+            }
+            break;
+    case 0:
+        Entity * h = NULL;
+        printf("que voulez ajoutez comme Humain");
+        printf("1 alien distance");
+        prinf("2 alien melee");
+        printf("3 alien tank");
+        printf("4 alien tank");
+        switch (ch2)
+        {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        }
+        break;
+    default:
+        fprintf(stderr,"ERROR in add_entity");
+        break;
+    }
+}
--- src/interaction.h
+++ src/interaction.h
@@ -2,22 +2,18 @@
 #include <stdlib.h>
 
 
-// function which manage Table Human
-Human * initTab_Human ();
+// function which init Table Human
+Entity *** initTab_Human ();
 
+// function which init Table Alien
+Entity *** initTab_Alien ();
 
-
-
-
-// function which manage Table Alien
-Alien * initTab_Alien ();
-
-
-
+// function which add a Entity structure in Alien_Tab or Human_Table
+void add_entity ();
 
 
 
 // function which manage interaction between Alien and Human
 
-void search (Human * Tab_Human, Alien * Tab_Alien);
-void attack (Human * human, Alien * alien);
+void search (Entity * Tab_Human, Entity * Tab_Alien);
+void attack (Entity * human, Entity * alien);
