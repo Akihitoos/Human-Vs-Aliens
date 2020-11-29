7ab57be76d9b7b26562715ec0b47b19157790207
Akihitoos <antoinsta@gmail.com>
2020-11-29 11:24:17 +0100

    Added function definition in interaction.h

--- src/interaction.h
+++ src/interaction.h
@@ -22,9 +22,25 @@ void add_entity_to_lane(Entity *lane, Entity *new);
 // create an Entity (depending on the id) and add it to a simple array of entity
 void add_entity(Entity **simpleArray, int id, int lane, int pos);
 
-// function which manage interaction between Alien and Human
+// Delete an entity on the lane (using it's pointer)
+void delete_entity_on_lane(Entity **simpleArray,int lane, Entity *todelete);
+
+// Free an entire lane, by passing the first element
+void free_tab_content_recursive(Entity *entity);
+
+// Free the two array dealing with the Entity on the board (for the end game)
+void free_array(Entity **Human, Entity **Alien);
+
+// Function wich return the entity closest to the ALien in it's range
 Entity* alien_search_human (Entity * Alien, Entity * tab_human);
+
+// Function wich return the entity closest to the Human in it's range
 Entity* human_search_alien (Entity * human, Entity * tab_alien);
+
+//Inflict damage of the attacker to the attacked
 void attack (Entity *attacker, Entity *attacked);
+
+// Make the entity move depending on his movement_speed
+void move(Entity *alien);
 
 #endif
