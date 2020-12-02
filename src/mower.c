8f70964dd943dab694324d03075e8491987ce37e
Taumicron <theo.lorthioir@hotmail.fr>
2020-12-02 11:28:27 +0100

    added mower

---
+++ src/mower.c
@@ -0,0 +1,37 @@
+#include "mower.h"
+
+
+//Returns a pointer to the mower tab.
+Mower init_mower_tab()
+{
+    Mower tab = (Mower)malloc(sizeof(Mower)*LANE);
+    for (int i =0;i<LANE;i++){
+        *(tab+i) = true;
+    }
+    return tab;
+}
+
+//Returns 1 if mower activated, 0 if game ends. If return 1, free the corresponding lane, and disables the mower.
+int activate_mower(Mower tab,int lane, Entity **entity_tab){
+    if (tab[lane]){
+        tab[lane] = false;
+        free_tab_content_recursive(entity_tab[lane]);
+        return 1;
+    }
+    else
+    {
+        //Game ended
+        return 0;
+    }
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    Mower tab = init_mower_tab();
+    for (int i = 0;i<LANE;i++){
+        printf("GROPD %d\n",tab[i]);
+    }
+    return 0;
+}
+
